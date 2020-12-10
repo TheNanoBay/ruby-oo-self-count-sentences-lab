e47e667bc73ed2c7a571273d78b181b4ab6324e0
TheNanoBay <noorani.mehdi@outlook.com>
2020-12-10 09:14:55 -0600

    done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
+    if self.string.end_with?(".") == true 
+      return true 
+    else
+      return false
+    end
     
-  end
 
   def question?
+    if self.string.end_with?("?") == true 
+      return true 
+    else
+      return false
+    end
 
   end
 
   def exclamation?
-
+    if self.string.end_with?("!") == true 
+      return true 
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
